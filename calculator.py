0ca242d5cadd70d823b67fbe25caf9000f712680
dynilath <dynilath@qq.com>
2024-01-07 10:41:56 +0800

    Basic deduction solver

--- calculator.py
+++ calculator.py
@@ -17,10 +17,11 @@ class GameState:
         for v in self.possible_codes:
             # for a specific criteria group, there must be at least one criterion that is satisfied
             goodv = True
-            for idx in self.activated_group_idx:
-                if not any(map(lambda cri: cri(v), criteria[idx])):
-                    goodv = False
-                    break
+            for i in range(len(self.activated_group_idx)):
+                group = criteria[self.activated_group_idx[i]]
+                possible_cris = self.possible_criteria_combination[i]
+                goodv = goodv and any(
+                    map(lambda i: group[i](v), possible_cris))
             if goodv:
                 filtered.append(v)
         self.possible_codes = filtered
@@ -45,6 +46,7 @@ class GameState:
     def print(self):
         GREY = '\033[90m'
         WHITE = '\033[0m'
+        GREEN = '\033[92m'
 
         print(WHITE + "Possible criteria combination:")
         for i in range(len(self.activated_group_idx)):
@@ -55,7 +57,7 @@ class GameState:
                 if not j == 0:
                     print(WHITE + " | ", end="")
                 if j in self.possible_criteria_combination[i]:
-                    print(WHITE + "%s" % (group[j]), end="")
+                    print(GREEN + "%s" % (group[j]), end="")
                 else:
                     print(GREY + "%s" % (group[j]), end="")
             print("")
@@ -67,7 +69,7 @@ class GameState:
                 if not i == 1:
                     print(WHITE + " | ", end="")
                 if i in l:
-                    print(WHITE + "%d" % i, end="")
+                    print(GREEN + "%d" % i, end="")
                 else:
                     print(GREY + "%d" % i, end="")
             print("")
@@ -81,9 +83,43 @@ class GameState:
         print(WHITE + "Purple:\t", end="")
         print_color(set(map(lambda c: c[2], self.possible_codes)))
 
+        print(WHITE)
+
+    def put_result(self, input: InputType, which: int, result: bool):
+        target = self.activated_group_idx.index(which)
+        group = criteria[which]
+        filtered = []
+        for j in self.possible_criteria_combination[target]:
+            if group[j](input) == result:
+                filtered.append(j)
+        self.possible_criteria_combination[target] = filtered
+
+        self.remove_impossible_codes()
+        self.remove_impossible_criteria_combinations()
+        self.remove_impossible_codes()
+
 
 if __name__ == "__main__":
-    a = input("Input activated groups (separated by \",\"): ")
+    a = input("Input activated criteria (separated by \",\"): ")
     groups = [int(i)-1 for i in a.split(',')]
     gs = GameState(groups)
     gs.print()
+
+    while True:
+        a = input(
+            "Input code, criteria and result (separated by \",\", use O/X for result): ")
+        if a == 'exit':
+            break
+        code1, code2, code3, whi, result = a.split(',')
+        code = (int(code1), int(code2), int(code3))
+        which = int(whi) - 1
+        result = result.lower()
+        if result == 'o':
+            result = True
+        elif result == 'x':
+            result = False
+        else:
+            print("Invalid input!")
+            continue
+        gs.put_result(code, which, result)
+        gs.print()
